f860ca9edc2b150c49315c1870d814aca8e4fcd2
Enrico Gollner <102383144+EnricoGollner@users.noreply.github.com>
2022-11-04 02:15:12 -0300

    1.0

---
+++ GuessTheFlag/GuessTheFlagApp.swift
@@ -0,0 +1,17 @@
+//
+//  GuessTheFlagApp.swift
+//  GuessTheFlag
+//
+//  Created by Enrico Sousa Gollner on 02/11/22.
+//
+
+import SwiftUI
+
+@main
+struct GuessTheFlagApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
